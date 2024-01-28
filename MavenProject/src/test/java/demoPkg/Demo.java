18acf2383c3a07d79aba459b75a5bf04009d00ba
karimunnisa12345 <arshiyajyothi@gmail.com>
2024-01-28 17:14:50 +0530

    First Commit

---
+++ MavenProject/src/test/java/demoPkg/Demo.java
@@ -0,0 +1,21 @@
+package demoPkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	WebDriver driver;
+	@Test
+	public void operation()
+	{
+		//WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		
+	}
+	
+}
